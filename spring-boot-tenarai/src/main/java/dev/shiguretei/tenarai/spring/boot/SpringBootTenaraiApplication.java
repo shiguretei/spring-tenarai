da3d4c2624d1e0a1d6e3558d948052ba375a6d50
shiguretei <shiguretei.dev@gmail.com>
2021-05-19 00:40:59 +0900

    spring-boot-tenarai init

---
+++ spring-boot-tenarai/src/main/java/dev/shiguretei/tenarai/spring/boot/SpringBootTenaraiApplication.java
@@ -0,0 +1,13 @@
+package dev.shiguretei.tenarai.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTenaraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTenaraiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-tenarai/src/test/java/dev/shiguretei/tenarai/spring/boot/SpringBootTenaraiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.shiguretei.tenarai.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTenaraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
